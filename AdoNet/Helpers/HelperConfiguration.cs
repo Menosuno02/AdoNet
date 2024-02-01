9b9fd60f4ae16934785b0965f85da778aac29e6f
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-01 11:04:00 +0100

    Commit

---
+++ AdoNet/Helpers/HelperConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNet.Helpers
+{
+    public class HelperConfiguration
+    {
+        public static string GetConnectionString()
+        {
+            // Debemos cargar el file config.json
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("config.json", true, true);
+
+            // Recuperamos la clase que proporciona los values del JSON
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration["SqlHospital"];
+            return connectionString;
+        }
+    }
+}
